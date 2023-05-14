97e4d3933f5e6d9d25d967b95f8a928674f1f84f
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-14 15:40:29 +0700

    Update kalkulator.sh

--- kalkulator.sh
+++ kalkulator.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
+
+echo "================================="
+echo "| Selamat datang di Kalkulator | "
+echo "================================="
+echo "Silahkan pilih operasi yang ingin dilakukan:"
+echo "1. Penjumlahan"
+echo "2. Pengurangan"
+echo "3. Perkalian"
+echo "4. Pembagian"
+read -p "Pilihan Anda (1/2/3/4): " pilihan
+
+read -p "Masukkan bilangan pertama: " bilangan1
+read -p "Masukkan bilangan kedua: " bilangan2
+
+if [ $pilihan -eq 1 ]; then
+  hasil=$((bilangan1 + bilangan2))
+  echo "Hasil penjumlahan: $hasil"
+elif [ $pilihan -eq 2 ]; then
+  hasil=$((bilangan1 - bilangan2))
+  echo "Hasil pengurangan: $hasil"
+elif [ $pilihan -eq 3 ]; then
+  hasil=$((bilangan1 * bilangan2))
+  echo "Hasil perkalian: $hasil"
+elif [ $pilihan -eq 4 ]; then
+  hasil=$((bilangan1 / bilangan2))
+  echo "Hasil pembagian: $hasil"
+else
+  echo "Pilihan tidak valid"
+fi
 
